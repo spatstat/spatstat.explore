d6e4d2873a2624f419404148f997470bd1c3d1d6
Adrian Baddeley <adrian.baddeley@curtin.edu.au>
2022-06-09 09:35:58 +0800

    marktable, applynbd handle pp3, lpp

--- R/marktable.R
+++ R/marktable.R
@@ -4,7 +4,7 @@
 #	Tabulate mark frequencies in neighbourhood of each point 
 #	for multitype point patterns
 #
-#	$Revision: 1.7 $	$Date: 2015/03/25 03:43:35 $
+#	$Revision: 1.8 $	$Date: 2022/06/09 01:07:54 $
 #
 #       Requested by Ian Robertson <igr@stanford.edu>
 
@@ -12,18 +12,20 @@
 "marktable" <- 
 function(X, R, N, exclude=TRUE, collapse=FALSE) 
 {
-  verifyclass(X, "ppp")
+  if(!inherits(X, c("ppp", "lpp", "pp3", "ppx")))
+    stop("X should be a point pattern (of class 'ppp', 'lpp', 'pp3' or 'ppx')")
   if(!is.marked(X, dfok=FALSE))
     stop("point pattern has no marks")
+  m <- marks(X)
+  if(!is.factor(m))
+    stop("marks must be a factor")
+
   gotR <- !missing(R) && !is.null(R)
   gotN <- !missing(N) && !is.null(N)
   if(gotN == gotR)
     stop("Exactly one of the arguments N and R should be given")
   stopifnot(is.logical(exclude) && length(exclude) == 1)
 
-  m <- marks(X)
-  if(!is.factor(m))
-    stop("marks must be a factor")
 
   if(gotR) {
     stopifnot(is.numeric(R) && length(R) == 1 && R > 0)
