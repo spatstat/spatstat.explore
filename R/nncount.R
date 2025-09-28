e87319af82629750861fcdd5e34e2cc9e871d8f6
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2025-09-28 11:43:53 +0800

    New functions nnequal, nncount, tolcon

---
+++ R/nncount.R
@@ -0,0 +1,115 @@
+#' nncount.R
+#'
+#' Functions nncount() and nnequal()
+#' 
+#' Copyright (c) 2019-2025 Lucia Cobo Sanchez and Adrian Baddeley
+#' GNU Public Licence >= 2.0
+#'
+#' $Revision: 1.1 $ $Date: 2025/09/28 03:11:58 $
+
+nncount <- function(X, i=1, j=2, ..., kmax=20, ratio=TRUE, cumulative=TRUE) {
+  stopifnot(is.ppp(X))
+  stopifnot(is.multitype(X))
+  marx <- marks(X)
+  lev <- levels(marx)
+  if(is.numeric(i)) i <- lev[i]
+  if(is.numeric(j)) j <- lev[j]
+  if(is.na(match(i, lev)))
+    stop(paste("Unrecognised value", i, "for argument i"))
+  if(is.na(match(j, lev)))
+    stop(paste("Unrecognised value", j, "for argument j"))
+  iname <- make.parseable(paste(i))
+  jname <- make.parseable(paste(j))
+  N <- nnwhich(X, k=1:kmax)
+  mi <- (marx == i)
+  mj <- (marx == j)
+  if(!any(mi)) {
+    numer <- denom <- rep(0, kmax)
+  } else if(!any(mj)) {
+    numer <- rep(0, kmax)
+    denom <- sum(mi)
+  } else {
+    Xsub <- X[mi]
+    Nsub <- N[mi, , drop=FALSE]
+    Dsub <- nndist(Xsub,  k=1:kmax)
+    Bsub <- bdist.points(Xsub)
+    observed <- (Dsub <= Bsub)
+    counted <- matrix(mj[Nsub], ncol=kmax)
+    if(cumulative) {
+      numer <- rowSums(apply(observed & counted, 1, cumsum))
+      denom <- rowSums(apply(observed,           1, cumsum))
+    } else {
+      numer <- colSums(observed & counted)
+      denom <- colSums(observed)
+    }
+  }
+  estimate <- ifelse(denom > 0, numer/denom, 0)
+
+  pj <- mean(mj)
+  df <- data.frame(k=1:kmax, theo=pj, bord=estimate)
+  desc <- c("neighbour order k",
+            "theoretical %s", 
+            "border-corrected estimate of %s")
+  labl <- c("k","{%s[%s]^{theo}}(k)", "{hat(%s)[%s]^{bord}}(k)")
+  dendf <- data.frame(k=1:kmax, theo=denom, bord=denom)
+  ylab <- substitute(N[i,j](r), list(i=iname,j=jname))
+  yexp <- substitute(N[list(i,j)](k), list(i=iname,j=jname))
+  fname <- c("N", paste0("list(", iname, ",", jname, ")"))
+  Z <- ratfv(df, NULL, dendf, 
+             argu="k",
+             ylab=ylab,
+             valu="bord",
+             fmla = . ~ k,
+             alim=c(1,kmax),
+             labl=labl, desc=desc, fname=fname, yexp=yexp,
+             ratio=ratio,
+             unitname=c("neighbour step", "neighbour steps"))
+  ##
+  return(Z)
+}
+
+nnequal <- function(X, ..., kmax=20, ratio=TRUE, cumulative=TRUE) {
+  stopifnot(is.ppp(X))
+  stopifnot(is.multitype(X))
+  N <- nnwhich(X, k=1:kmax)
+  D <- nndist(X,  k=1:kmax)
+  B <- bdist.points(X)
+  observed <- (D <= B)
+  marx <- marks(X)
+  mI <- matrix(marx[row(N)], ncol=kmax)
+  mJ <- matrix(marx[N], ncol=kmax)
+  counted <- (mI == mJ)
+  if(cumulative) {
+    numer <- rowSums(apply(observed & counted, 1, cumsum))
+    denom <- rowSums(apply(observed,           1, cumsum))
+  } else {
+    numer <- colSums(observed & counted)
+    denom <- colSums(observed)
+  }
+  estimate <- ifelse(denom > 0, numer/denom, 0)
+
+  m <- as.integer(table(marx))
+  n <- npoints(X)
+  pequal <- sum(m*(m-1)/(n*(n-1)))
+  
+  df <- data.frame(k=1:kmax, theo=pequal, bord=estimate)
+  desc <- c("neighbour order k",
+            "theoretical %s", 
+            "border-corrected estimate of %s")
+  labl <- c("k","%s[theo](k)", "hat(%s)[bord](k)")
+  dendf <- data.frame(k=1:kmax, theo=denom, bord=denom)
+  yexp <- ylab <- quote(E(k))
+  fname <- "E"
+  Z <- ratfv(df, NULL, dendf, 
+             argu="k",
+             ylab=ylab,
+             valu="bord",
+             fmla = . ~ k,
+             alim=c(1,kmax),
+             labl=labl, desc=desc, fname=fname, yexp=yexp,
+             ratio=ratio,
+             unitname=c("neighbour step", "neighbour steps"))
+  ##
+  return(Z)
+}
+
---
+++ R/tolcon.R
@@ -0,0 +1,96 @@
+#'
+#' tolcon.R
+#' 
+#' Generate tolerance contours for relative risk or conditional probability
+#'
+#' Copyright (c) 2019-2025 Adrian Baddeley and Tilman Davies
+#' GNU Public Licence >= 2.0
+#'
+#' $Revision: 1.3 $ $Date: 2025/09/28 03:23:03 $
+
+tolcon <- function(X, ..., nsim=19,
+                   alternative=c("greater", "less", "two.sided"),
+                   verbose=TRUE) {
+  stopifnot(is.ppp(X))
+  stopifnot(is.marked(X))
+  check.1.integer(nsim)
+  alternative <- match.arg(alternative)
+  Zdata <- as.solist(relrisk(X, ...))
+  n <- length(Zdata)
+  rankcount <- rep(list(0), n)
+  if(verbose) 
+    cat(paste("Simulating", nsim, "random labellings..."))
+  pstate <- list()
+  for(i in seq_len(nsim)) {
+    Xsim <- rlabel(X)
+    Zsim <- as.solist(relrisk(Xsim, ...))
+    if(length(Zsim) != n)
+      stop("Different numbers of images produced in data and simulation")
+    for(j in seq_len(n)) 
+      rankcount[[j]] <- rankcount[[j]] + (Zsim[[j]] >= Zdata[[j]])
+    if(verbose)
+      pstate <- progressreport(i, nsim, state=pstate)
+  }
+  if(verbose) 
+    splat("Done.")
+  result <- Zdata
+  for(j in seq_len(n)) {
+    y <- result[[j]]
+    r <- rankcount[[j]]
+    attr(y, "pvalues") <-
+      switch(alternative,
+             greater = { (r+1)/(nsim+1.0) },
+             less    = { (nsim-r+1)/(nsim+1.0) },
+             two.sided = {
+               pg <- (r+1)/(nsim+1.0)
+               pl <- (nsim-r+1)/(nsim+1.0)
+               eval.im(pmin(1, 2*pmin(pg, pl)))
+             })
+    attr(y, "nsim") <- nsim
+    attr(y, "alternative") <- alternative
+    class(y) <- c("tolcon", class(y))
+    result[[j]] <- y
+  }
+  class(result) <- c("tolconlist", class(result))
+  return(result)
+}
+
+shift.tolcon <- function(X, ...) {
+  y <- shift.im(X, ...)
+  attr(y, "pvalues") <- shift.im(attr(X, "pvalues"), ...)
+  return(y)
+}
+
+shift.tolconlist <- function(X, ...) {
+  y <- solapply(X, shift, ...)
+  class(y) <- union("tolconlist", class(y))  
+  return(y)
+}
+
+plot.tolconlist <- function(x, ...) {
+  plot.imlist(x, ..., plotcommand=plot.tolcon)
+}
+
+plot.tolcon <- function(x, ...,
+                        show.contour=TRUE,
+                        levels=0.05) {
+  result <- plot.im(x, ...)
+  if(show.contour)
+    do.call.matched(contour.im,
+                    resolve.defaults(list(x=attr(x, "pvalues"),
+                                          levels=levels * 1.0001,
+                                          add=TRUE),
+                                     list(...),
+                                     list(col="white",
+                                          drawlabels=FALSE)),
+                    extrargs=names(formals(contour.default)))
+  return(result)
+}
+
+print.tolcon <- function(x, ...) {
+  print.im(x, ...) 
+  splat("\t[Includes tolerance p-values based on",
+        attr(x, "nsim"),
+        "random labellings]")
+  return(invisible(NULL))
+}
